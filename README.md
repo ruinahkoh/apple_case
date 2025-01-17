e76345ef1c9ee5fa525d1345dbbd03327fa336df
Rui Nah Koh <ruinah.koh@gmail.com>
2025-01-17 16:03:22 +0100

    add changes to build vectorstore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# llm_prompting
+# News Search application demonstrationg RAG
+This repo demonstrates the ability to use Retrival Augmented Generation to search for tech news articles.
+
+1. News articles are collected using the newspaper-api
+2. Data Processing and feature selection
+3. Constructing a vectordb using Chroma
+4. Using ChatGPT to query vectordb and return relevant news articles
+4. Streamlit Front end to add user input 
+
--- build_vectorstore.py
+++ build_vectorstore.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import json
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 import streamlit as st
 from src.utils import *
+
+##
+from functools import partial
 
 
 with open('src/config.json', 'r') as f:
@@ -14,16 +17,18 @@ with open('src/config.json', 'r') as f:
 
 file_path = config["vectorstore"]["file_path"]
 batch_size = config["vectorstore"]["batch_size"]   
-collection_name = config["vectorstore"]["collection_name"]  
 
-def add_to_chroma_database(batch):
+
+
+def add_to_chroma_database(batch, vectordb):
     vectordb.add_documents(documents=batch)
 
 # file = 'data/tech_news_articles.csv'
-@st.cache_resource
+
 def generate_data_store():
     """Function to generate chroma vector store uing documents"""
- 
+
+
     documents = load_data(file_path)
     print(len(documents))
     # define embeddings
@@ -32,14 +37,19 @@ def generate_data_store():
     # #Place vectorDB under /tmp. It can be anywhere else
     # # from langchain.vectorstores import Chroma
     persist_directory = config["vectorstore"]["persist_directory"]
-    vectordb = Chroma(embedding_function =embeddings,
+    collection_name = config["vectorstore"]["collection_name"]  
+    vectordb = Chroma(collection_name=collection_name, embedding_function=embeddings,
                                  persist_directory=persist_directory)
-    # vectordb = Chroma.from_documents(documents=list(documents[0:1]), embedding=embeddings,
+    # vectordb = Chroma.from_documents(documents=list(documents[0:100]), embedding=embeddings,
     #                              persist_directory=persist_directory)
 
     data_list = form_batch(documents, batch_size)
+    add_to_chroma_database_with_param = partial(add_to_chroma_database, vectordb=vectordb)
 
-    save_to_chroma(data_list)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            executor.map(add_to_chroma_database_with_param,  data_list)
+
+    # save_to_chroma(data_list)
 
     print(vectordb._collection.count())
 
